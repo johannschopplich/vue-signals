1285cf8282cc035ddf48cf1bf36678f9178e4b08
Johann Schopplich <mail@johannschopplich.com>
2023-02-24 14:35:21 +0100

    chore: update playground with Angular signal example

--- playground/App.vue
+++ playground/App.vue
@@ -1,12 +1,41 @@
 <script lang="ts" setup>
-import { createSignal } from 'vue-signals'
+import { computed, createSignal, signal } from 'vue-signals'
 
+// Solid-style
 const [count, setCount] = createSignal(0)
+
+// Angular-style
+const angularCount = signal(0)
+const angularDouble = computed(() => count() * 2)
 </script>
 
 <template>
-  <h1>{{ count() }}</h1>
-  <button @click="setCount((v) => v + 1)">
-    increment
-  </button>
+  <header>
+    <h1>vue-signals</h1>
+  </header>
+
+  <h2>Flavour: Solid</h2>
+  <p>
+    <code>Count: {{ count() }}</code>
+  </p>
+  <p>
+    <button @click="setCount((v) => v + 1)">
+      increment
+    </button>
+  </p>
+
+  <h2>Flavour: Angular</h2>
+  <p>
+    Count: {{ angularCount() }}<br>
+    Double: {{ angularDouble() }}
+  </p>
+  <p>
+    <button @click="angularCount.update(v => v + 1)">
+      increment
+    </button>
+    &nbsp;
+    <button @click="angularCount.set(0)">
+      reset
+    </button>
+  </p>
 </template>
